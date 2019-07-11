7f8290f57fb47c0f30e3f48f23a15df18a993739
Qi Zhang <singularitti@outlook.com>
2019-07-11 00:00:10 -0400

    docs: Update build settings

--- docs/make.jl
+++ docs/make.jl
@@ -5,6 +5,9 @@ makedocs(;
     format=Documenter.HTML(),
     pages=[
         "Home" => "index.md",
+        "Manual" => Any[
+            "EigenvalueProblems/NumerovMethod.md",
+        ],
     ],
     repo="https://github.com/singularitti/NumericalMethodsInQuantumMechanics.jl/blob/{commit}{path}#L{line}",
     sitename="NumericalMethodsInQuantumMechanics.jl",
