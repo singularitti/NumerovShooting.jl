a5ddbbac8195cd6f8d31540151c3ffe29d28d2e9
Qi Zhang <singularitti@outlook.com>
2019-07-11 02:40:06 -0400

    Add type promotion to `Condition`s

--- src/EigenvalueProblems/Conditions.jl
+++ src/EigenvalueProblems/Conditions.jl
@@ -20,10 +20,12 @@ struct InitialCondition{T} <: FieldVector{2, T}
     y0::T
     yd0::T
 end
+InitialCondition(y0::Real, yd0::Real) = InitialCondition(promote(y0, yd0)...)
 
 struct BoundaryCondition{T} <: FieldVector{2, T}
     y0::T
     y1::T
 end
+BoundaryCondition(y0::Real, y1::Real) = BoundaryCondition(promote(y0, y1)...)
 
 end
