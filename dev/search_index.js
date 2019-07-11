0ebb7bfb256581227f4481fcae4e80ce43a494a6
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-07-11 03:49:26 +0000

    build based on 1bf3b37

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#NumericalMethodsInQuantumMechanics.jl-1","page":"Home","title":"NumericalMethodsInQuantumMechanics.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [NumericalMethodsInQuantumMechanics]","category":"page"}]
+}
