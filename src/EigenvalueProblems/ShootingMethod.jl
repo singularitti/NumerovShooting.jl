b23b37cb834c5748bdf75f086717b938206473a9
singularitti <singularitti@outlook.com>
2020-12-18 01:41:37 -0500

    Merge `setup_problem` & `shoot`

--- src/EigenvalueProblems/ShootingMethod.jl
+++ src/EigenvalueProblems/ShootingMethod.jl
@@ -15,17 +15,16 @@ using Roots: find_zero, AbstractUnivariateZeroMethod
 
 using NumericalMethodsInQuantumMechanics.EigenvalueProblems:
     InitialCondition, BoundaryCondition
-using NumericalMethodsInQuantumMechanics.EigenvalueProblems.NumerovMethod
+using NumericalMethodsInQuantumMechanics.EigenvalueProblems.NumerovMethod: integrate
 
-export setup_problem, shoot
+export shoot
 
 # last means ``y(t1)``, `ic` contains a guess, `bc[2]` is ``y1``.
 # `last(integrate(ic_guess, r, g, s)) - bc[2]` means ``y(t1; yd0) - y1``.
-setup_problem(bc::BoundaryCondition, r, g, s) =
-    guess -> last(integrate(InitialCondition(bc.y0, guess), r, g, s)) - bc.y1
-
-# `x` of the root finding is `y′0`.
-shoot(f, method::AbstractUnivariateZeroMethod, r) =
-    find_zero(f, (minimum(r), maximum(r)), method)
+function shoot(bc::BoundaryCondition, g, s, init, args...)
+    f = guess -> last(integrate(InitialCondition(bc.y0, guess), g, s, args...)) - bc.y1
+    # `x` of the root finding is `y′0`.
+    return find_zero(f, init)
+end
 
 end
