5163aeaa6e40f118b861d41be6700d38e35d2b68
singularitti <singularitti@outlook.com>
2020-12-17 21:24:34 -0500

    Simplify the definition of `InitialCondition` & `BoundaryCondition`

--- src/EigenvalueProblems/EigenvalueProblems.jl
+++ src/EigenvalueProblems/EigenvalueProblems.jl
@@ -15,15 +15,10 @@ using Compat: @NamedTuple
 
 export InitialCondition, BoundaryCondition
 
-const InitialCondition = @NamedTuple begin
-    y0
-    y′0
-end
+const InitialCondition = @NamedTuple{y0, y′0}
 InitialCondition(y0, y′0) = InitialCondition((y0, y′0))
-const BoundaryCondition = @NamedTuple begin
-    y0
-    y1
-end
+
+const BoundaryCondition = @NamedTuple{y0, y1}
 BoundaryCondition(y0, y1) = BoundaryCondition((y0, y1))
 
 include("NumerovMethod.jl")
