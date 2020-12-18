8f8bfabef450f48df54c5eb03394915df94f7a6c
singularitti <singularitti@outlook.com>
2020-12-17 21:13:12 -0500

    Deprecate Conditions.jl, move to src/EigenvalueProblems/EigenvalueProblems.jl

--- src/EigenvalueProblems/EigenvalueProblems.jl
+++ src/EigenvalueProblems/EigenvalueProblems.jl
@@ -11,7 +11,21 @@ julia>
 """
 module EigenvalueProblems
 
-include("Conditions.jl")
+using Compat: @NamedTuple
+
+export InitialCondition, BoundaryCondition
+
+const InitialCondition = @NamedTuple begin
+    y0
+    y′0
+end
+InitialCondition(y0, y′0) = InitialCondition((y0, y′0))
+const BoundaryCondition = @NamedTuple begin
+    y0
+    y1
+end
+BoundaryCondition(y0, y1) = BoundaryCondition((y0, y1))
+
 include("NumerovMethod.jl")
 include("ShootingMethod.jl")
 
