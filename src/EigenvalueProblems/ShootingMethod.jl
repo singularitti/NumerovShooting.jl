551dac3dd5b86e6b415947ce0ef85f6e15f4dac4
singularitti <singularitti@outlook.com>
2020-12-18 00:16:59 -0500

    Simplify `setup_problem` & `shoot`

--- src/EigenvalueProblems/ShootingMethod.jl
+++ src/EigenvalueProblems/ShootingMethod.jl
@@ -19,17 +19,13 @@ using NumericalMethodsInQuantumMechanics.EigenvalueProblems.NumerovMethod
 
 export setup_problem, shoot
 
-function setup_problem(bc::BoundaryCondition, r, g, s)
-    function (guess)
-        # last means ``y(t1)``, `ic` contains a guess, `bc[2]` is ``y1``.
-        # `last(integrate(ic_guess, r, g, s)) - bc[2]` means ``y(t1; yd0) - y1``.
-        return last(integrate(InitialCondition(bc[1], guess), r, g, s)) - bc[2]
-    end
-end  # function setup_problem
+# last means ``y(t1)``, `ic` contains a guess, `bc[2]` is ``y1``.
+# `last(integrate(ic_guess, r, g, s)) - bc[2]` means ``y(t1; yd0) - y1``.
+setup_problem(bc::BoundaryCondition, r, g, s) =
+    guess -> last(integrate(InitialCondition(bc.y0, guess), r, g, s)) - bc.y1
 
-function shoot(f::Function, method::AbstractUnivariateZeroMethod, r)::Real
-    # `x` of the root finding is `yd0`.
-    return find_zero(f, (minimum(r), maximum(r)), method)
-end  # function shoot
+# `x` of the root finding is `y′0`.
+shoot(f, method::AbstractUnivariateZeroMethod, r) =
+    find_zero(f, (minimum(r), maximum(r)), method)
 
 end
