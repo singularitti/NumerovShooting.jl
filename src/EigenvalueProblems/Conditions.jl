b95379d985cbf649b4984ed31eaed9cc4d3c88f4
singularitti <singularitti@outlook.com>
2020-12-17 20:44:31 -0500

    Rewrite `InitialCondition` & `BoundaryCondition` using `NamedTuple` rather than `FieldVector`

--- src/EigenvalueProblems/Conditions.jl
+++ src/EigenvalueProblems/Conditions.jl
@@ -11,26 +11,17 @@ julia>
 """
 module Conditions
 
-using StaticArrays: FieldVector
+using Compat: @NamedTuple
 
 export InitialCondition, BoundaryCondition
 
-struct InitialCondition{T} <: FieldVector{2,T}
-    y0::T
-    yd0::T
+const InitialCondition = @NamedTuple begin
+    y0
+    y′0
 end
-function InitialCondition(y0::Real, yd0::Real)
-    T = Base.promote_typeof(y0, yd0)
-    InitialCondition{T}(y0, yd0)
-end
-
-struct BoundaryCondition{T} <: FieldVector{2,T}
-    y0::T
-    y1::T
-end
-function BoundaryCondition(y0::Real, y1::Real)
-    T = Base.promote_typeof(y0, y1)
-    BoundaryCondition{T}(y0, y1)
+const BoundaryCondition = @NamedTuple begin
+    y0
+    y1
 end
 
 end
