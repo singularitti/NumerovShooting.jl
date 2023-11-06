234941d6380e2c79bdc369bc5ad9d5d91d01adba
singularitti <singularitti@outlook.com>
2023-11-06 05:25:18 -0500

    Add plotting in examples/chapter1_zhihu.jl

--- examples/chapter1_zhihu.jl
+++ examples/chapter1_zhihu.jl
@@ -1,11 +1,31 @@
 # This simulation is referenced from
 # [《量子力学本征值问题的数值方法——打靶法与Numerov方法》](https://zhuanlan.zhihu.com/p/59099100).
 using LinearAlgebra: normalize
+using Plots
 using Roots
 
 using NumericalMethodsInQuantumMechanics.EigenvalueProblems
 using NumericalMethodsInQuantumMechanics.EigenvalueProblems.NumerovIntegrator
 using NumericalMethodsInQuantumMechanics.EigenvalueProblems.ShootingMethod
+
+PLOT_DEFAULTS = Dict(
+    :size => (800, 450),
+    :framestyle => :box,
+    :linewidth => 1,
+    :markersize => 2,
+    :markerstrokewidth => 0,
+    :minorticks => 5,
+    :guidefontsize => 10,
+    :tickfontsize => 8,
+    :legendfontsize => 7,
+    :left_margin => (3.5, :mm),
+    :grid => nothing,
+    :legend_foreground_color => nothing,
+    :legend_background_color => nothing,
+    :legend_position => :outertop,
+    :legend_columns => 5,
+    :background_color_inside => nothing,
+)
 
 dx = 0.002
 x = range(0; stop=1, step=dx)
@@ -26,3 +46,9 @@ wavefuncs = map(energies) do energy
         ),
     )
 end
+plt = plot(; PLOT_DEFAULTS...)
+for (i, wavefunc) in enumerate(wavefuncs)
+    plot!(plt, x, wavefunc; ylims=extrema(wavefunc), label="$(i)th wavefunc")
+end
+xlims!(plt, extrema(x))
+display(plt)
