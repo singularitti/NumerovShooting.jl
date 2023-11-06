a64274ffb72e13f469fba6aa9f24aca62ac7a029
singularitti <singularitti@outlook.com>
2023-11-06 04:51:14 -0500

    Fix examples/chapter1_zhihu.jl

--- examples/chapter1_zhihu.jl
+++ examples/chapter1_zhihu.jl
@@ -1,19 +1,28 @@
 # This simulation is referenced from
 # [《量子力学本征值问题的数值方法——打靶法与Numerov方法》](https://zhuanlan.zhihu.com/p/59099100).
-using LinearAlgebra: dot
-
+using LinearAlgebra: normalize
 using Roots
-using SpecialMatrices: Strang
 
 using NumericalMethodsInQuantumMechanics.EigenvalueProblems
 using NumericalMethodsInQuantumMechanics.EigenvalueProblems.NumerovIntegrator
 using NumericalMethodsInQuantumMechanics.EigenvalueProblems.ShootingMethod
 
-x = range(0; stop = 1, step = 0.002)
+dx = 0.002
+x = range(0; stop=1, step=dx)
 n = length(x)
-g = zeros(n)
-s = zeros(n)
-problem = setup_problem(BoundaryCondition(0.0, 1.0), x, g, s)
-yd0 = shoot(problem, Bisection(), [0, 1000])
-ψ = integrate(InitialCondition(0.0, yd0), x, g, s)
-e = dot(ψ, Strang(n) * ψ)
+s = zeros(n);
+problem = Problem(identity, s, Dirichlet(0, 0), InitialCondition(0, 10), n, dx)
+exact_energies = Tuple(n^2 * π^2 / 2 for n in 1:5)
+energies = map(exact_energies) do g₀_init
+    shoot(problem, 2g₀_init, Order5()) / 2
+end
+errors = abs.(energies .- exact_energies)
+wavefuncs = map(energies) do energy
+    eval = 2energy
+    evec = normalize(
+        solve(
+            Problem(Returns(eval), s, Dirichlet(0, 0), InitialCondition(0, 10), n, dx),
+            Numerov(),
+        ),
+    )
+end
