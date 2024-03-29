622948c99a355ea024bc712516ff68a3c300e61a
singularitti <singularitti@outlook.com>
2024-03-29 19:22:11 -0400

    Fix docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -14,6 +14,23 @@ makedocs(;
     ),
     pages=[
         "Home" => "index.md",
+        "Manual" => [
+            "Installation Guide" => "man/installation.md",
+            "Definitions and conventions" => "man/definitions.md",
+            "Troubleshooting" => "man/troubleshooting.md",
+        ],
+        "Reference" => Any[
+            "Public API" => "lib/public.md",
+            # "Internals" => map(
+            #     s -> "lib/internals/$(s)",
+            #     sort(readdir(joinpath(@__DIR__, "src/lib/internals"))),
+            # ),
+        ],
+        "Developer Docs" => [
+            "Contributing" => "developers/contributing.md",
+            "Style Guide" => "developers/style-guide.md",
+            "Design Principles" => "developers/design-principles.md",
+        ],
     ],
 )
 
